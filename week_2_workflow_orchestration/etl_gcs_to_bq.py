45aa85e91e25e009c7ce6c60c70852fe47464f38
aliescont <aescontrela@gmail.com>
2023-02-08 17:48:11 +0000

    finishing homework week2

--- week_2_workflow_orchestration/etl_gcs_to_bq.py
+++ week_2_workflow_orchestration/etl_gcs_to_bq.py
@@ -5,39 +5,35 @@ from prefect_gcp.cloud_storage import GcsBucket
 from prefect_gcp import GcpCredentials
 
 
-@task(retries=3)
+@task(retries=3, log_prints=True)
 def extract_from_gcs(color: str, year: int, month: int) -> Path:
     """Download trip data from GCS"""
     gcs_path = f"data/{color}/{color}_tripdata_{year}-{month:02}.parquet"
     gcs_block = GcsBucket.load("de-zoomcamp")
-    gcs_block.get_directory(from_path=gcs_path, local_path=f"../data/")
-    return Path(f"../data/{gcs_path}")
+    gcs_block.get_directory(from_path=gcs_path)
+    return Path(f"{gcs_path}")
 
 
-@task()
+@task(log_prints=True)
 def write_bq(df: pd.DataFrame) -> None:
     """Write DataFrame to BiqQuery"""
 
     gcp_credentials_block = GcpCredentials.load("de-zoomcamp-gcp-creds")
-
+    print(f"rows: {len(df)}")
     df.to_gbq(
-        destination_table="trips_data_all.rides",
+        destination_table="trips_data_all.yellow_rides",
         project_id="dezoomcamp-2023-375020",
         credentials=gcp_credentials_block.get_credentials_from_service_account(),
         chunksize=500_000,
         if_exists="append",
     )
 
-
 @flow()
-def etl_gcs_to_bq():
+def etl_gcs_to_bq(year: int, month: int, color: str):
     """Main ETL flow to load data into Big Query"""
-    color = "yellow"
-    year = 2021
-    month = 1
-
     path = extract_from_gcs(color, year, month)
-    df = transform(path)
+    df = pd.read_parquet(path)
+    
     write_bq(df)
 
 @flow()
@@ -45,7 +41,11 @@ def etl_parent_flow(
     months: list[int] = [1, 2], year: int = 2021, color: str = "yellow"
 ):
     for month in months:
-        etl_web_to_gcs(year, month, color)
+        etl_gcs_to_bq(year, month, color)
+
 
 if __name__ == "__main__":
-    etl_gcs_to_bq()
+    color = 'yellow'
+    months = [2,3]
+    year = 2019
+    etl_parent_flow(months, year, color)
--- week_2_workflow_orchestration/hw.md
+++ week_2_workflow_orchestration/hw.md
@@ -41,7 +41,7 @@ Create a deployment for this flow to run in a local subprocess with local flow c
 
 Make sure you have the parquet data files for Yellow taxi data for Feb. 2019 and March 2019 loaded in GCS. Run your deployment to append this data to your BiqQuery table. How many rows did your flow code process?
 
-- 14,851,920
+- 14,851,920 -> 
 - 12,282,990
 - 27,235,753
 - 11,338,483
@@ -60,7 +60,7 @@ How many rows were processed by the script?
 
 - 88,019
 - 192,297
-- 88,605
+- 88,605 ->
 - 190,225
 
 
@@ -91,7 +91,7 @@ How many rows were processed by the script?
 - `125,268`
 - `377,922`
 - `728,390`
-- `514,392`
+- `514,392` ->
 
 
 ## Question 6. Secrets
@@ -100,7 +100,7 @@ Prefect Secret blocks provide secure, encrypted storage in the database and obfu
 
 - 5
 - 6
-- 8
+- 8 ->
 - 10
 
 
